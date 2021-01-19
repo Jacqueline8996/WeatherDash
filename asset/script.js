b8aeb2950cb61acfc7f4eca5ac038cdaf9695c70
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-01-18 22:10:04 -0500

    fixed tempature

--- asset/script.js
+++ asset/script.js
@@ -13,14 +13,40 @@ function searchHistory(cityname){
     $("#historyBtn").on("click", function (event) {
         var histBtn = $("#historyBtn").html();
         console.log("what is the town places",histBtn);
-
+        displaydaily(histBtn);
     })
+    
+
 
 }
+function displaydaily(cityname) {
+
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q="+cityname+"&appid="+apiKey+"&units=imperial";
+
+    console.log("what my URL",queryURL);
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+        var whatCity = $("<p>").html(cityname);
+        $(".dailybreakdown").append(whatCity);
+
+        var tempature = (response["list"]["0"]["main"]["temp"])+"Fs";
+        // $("#temp").text("temp: " + response.value)
+        console.log("what is the temp", response);
+        console.log("what is the temp", response["list"]["0"]["main"]["temp"]);
+        console.log("what is the temp", response);
+        // Creating an element to have the tempature displayed
+        var whatTemp = $("<p>")
+        var pOne = whatTemp.text("Tempature: " + tempature);
+        $(".dailybreakdown").append(pOne);
 
 
 
+    });
 
+}
 
 //Gets the name of the town 
 $("#searchBtn").on("click", function (event) {
@@ -28,7 +54,7 @@ $("#searchBtn").on("click", function (event) {
     var citySearch = $("#cityInput").val();
     console.log("what is the town places",citySearch);
     searchHistory(citySearch);
-    
+    displaydaily(citySearch);
 
 })
 
