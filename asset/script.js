e89d033d36f7be72faa6f882db7680717c2746e1
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-01-19 00:13:18 -0500

    added windspeed, UV index and humidity

--- asset/script.js
+++ asset/script.js
@@ -19,8 +19,32 @@ function searchHistory(cityname){
 
 
 }
-function displaydaily(cityname) {
 
+function uVData(latitude, longitude){
+
+    var uvUrL = "http://api.openweathermap.org/data/2.5/uvi?lat="+latitude+"&lon="+longitude+"&appid="+apiKey;
+
+    console.log("what my URL",uvUrL);
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+    url: uvUrL,
+    method: "GET"
+    }).then(function(response) {
+        console.log("my response", response)
+
+        var uvIndex = (response["value"]);
+        var whatUv = $("<p>")
+        var pFour = whatUv.text("UV Index: " + uvIndex );
+        //changes color for diffrent ranges of index
+        $(".dailybreakdown").append(pFour);
+
+    });
+}
+
+
+function displaydaily(cityname) {
+    
+    $(".dailyTitle").empty();
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q="+cityname+"&appid="+apiKey+"&units=imperial";
 
     console.log("what my URL",queryURL);
@@ -29,18 +53,40 @@ function displaydaily(cityname) {
     url: queryURL,
     method: "GET"
     }).then(function(response) {
-        var whatCity = $("<p>").html(cityname);
+        //title of the city and weather icon
+        var whatCity = $(".cityName").html(cityname);
         $(".dailybreakdown").append(whatCity);
+        var iconcode = response["list"][0]["weather"][0]["icon"]
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        $(".icon").append('<img id="theImg"' + "src="+ iconurl + ' />');
 
-        var tempature = (response["list"]["0"]["main"]["temp"])+"Fs";
-        // $("#temp").text("temp: " + response.value)
-        console.log("what is the temp", response);
-        console.log("what is the temp", response["list"]["0"]["main"]["temp"]);
-        console.log("what is the temp", response);
+        console.log("my response ", response);
         // Creating an element to have the tempature displayed
+        var tempature = (response["list"][0]["main"]["temp"]);
         var whatTemp = $("<p>")
-        var pOne = whatTemp.text("Tempature: " + tempature);
+        // var degree = $("<sup>")
+       
+        var pOne = whatTemp.text("Tempature: " + tempature + "F");
         $(".dailybreakdown").append(pOne);
+
+        //finds and adds humidity
+        var humidity = (response["list"][0]["main"]["humidity"]);
+        var whatHum = $("<p>")
+        var pTwo = whatHum.text("Humidity: " + humidity + "%");
+        $(".dailybreakdown").append(pTwo);
+
+        //find and add wind speed 
+        var windSpeed = (response["list"][0]["wind"]["speed"]);
+        var whatSpeed = $("<p>")
+        var pThree = whatSpeed.text("WindSpeed: " + windSpeed + "MPH");
+        $(".dailybreakdown").append(pThree);
+
+        //finds and adds UV Index 
+
+        var latiCo = (response["city"]["coord"]["lat"]);
+        var longCo = (response["city"]["coord"]["lon"]);
+
+        uVData(latiCo,longCo);
 
 
 
@@ -51,6 +97,7 @@ function displaydaily(cityname) {
 //Gets the name of the town 
 $("#searchBtn").on("click", function (event) {
     event.preventDefault();
+    // document.getElementById("weathericon").style.visibility = "hidden";
     var citySearch = $("#cityInput").val();
     console.log("what is the town places",citySearch);
     searchHistory(citySearch);
