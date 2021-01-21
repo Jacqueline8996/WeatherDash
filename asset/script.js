4d3a8ac2810eeda35a706c2fc1bf63fcc72aa874
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-01-21 00:52:00 -0500

    added temp and humidity to 5 day forcast

--- asset/script.js
+++ asset/script.js
@@ -66,7 +66,15 @@ function clearDiv(){
 
 }
 
-function fiveDay(latitude, longitude){
+function fiveDay(latitude,longitude,todaydate){
+
+    var whatTitle = $("<p>");
+    var pForTitle = whatTitle.html("5-DAY Forcast");
+    $("#fivedayTitle").append(pForTitle);
+    var dateVar = todaydate;
+    // var dateVar = [2021,01,31];
+    
+    console.log("what today date", dateVar)
 
     // "current+minutely,hourly,"
     var fiveURL = "http://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude=current+minutely,hourly,"+"&appid="+apiKey+"&units=imperial";
@@ -83,45 +91,40 @@ function fiveDay(latitude, longitude){
         $(whatTitleFor).html("date"+1)
 
 
-
+        //get the 5 day forcast
         for(i = 1; i < 6; i++) {
+            //getting date 
 
-            var iconcode = response["daily"][i]["weather"][0]["icon"]
+
+            // var whatDateForcast = "(" + dateVar[1]+ "/" + dateVar[2] + "/" + dateVar[0] + ")";
+            // $("#day"+i+"cast").append(whatDateForcast);
+            // console.log("what date for the forcast",whatDateForcast);
+
+            //get the icon 
+            var iconcode = response["daily"][i]["weather"][0]["icon"];
             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-            $("#day"+i+"cast").append('<img id="weatherImage"' + "src="+ iconurl + ' />');
+            $("#day"+i+"cast").append('<img id="weatherImage2"' + "src="+ iconurl + ' />');
+            
+            //get the tempature
+            console.log("my response ", response);
+            var tempaturefor = (response["daily"][i]["temp"]["day"]);
+            var whatTempfor = $("<p>")
+            var pForOne = whatTempfor.text("Temp: " + tempaturefor + " F");
+            $("#day"+i+"cast").append(pForOne);
 
 
+            //finds and adds humidity
+            var humidityFor = (response["daily"][i]["humidity"]);
+            var whatHumFor = $("<p>")
+            var pForTwo = whatHumFor.text("Humidity: " + humidityFor + "%");
+            $("#day"+i+"cast").append(pForTwo);
+
+            //add class for the forcast boxes
             $("#day"+i+"cast").addClass("forcast")
-            
-            
+
         }
         
-        //finding date for today
-        // var whatDateTime = response["list"][0]["dt_txt"];
-        // var whatDateWhole = whatDateTime.split(" ");
-        // var whatDateSplit = whatDateWhole[0].split("-");
-        // var whatDate = "(" + whatDateSplit[1]+ "/" + whatDateSplit[2] + "/" + whatDateSplit[0] + ")";
-        // $("#todayDate").append(whatDate);
-        // console.log("what date",whatDate);
-
-        // var iconcode = response["list"][0]["weather"][0]["icon"]
-        // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        // $("#icon").append('<img id="weatherImage"' + "src="+ iconurl + ' />');
-        
-        // console.log("my response ", response);
-        // Creating an element to have the tempature displayed
-        // var tempature = (response["list"][0]["main"]["temp"]);
-        // var whatTemp = $("<p>")
-        // var degree = $("<sup>")
-
-        // var pOne = whatTemp.text("Tempature: " + tempature + "F");
-        // $("#displayInfo").append(pOne);
-
-        //finds and adds humidity
-        // var humidity = (response["list"][0]["main"]["humidity"]);
-        // var whatHum = $("<p>")
-        // var pTwo = whatHum.text("Humidity: " + humidity + "%");
-        // $("#displayInfo").append(pTwo);
+      
     });
 
        
@@ -183,7 +186,7 @@ function displaydaily(cityname) {
         uVData(latiCo,longCo);
         
         // var cityId = (response["city"]["id"]);
-        fiveDay(latiCo,longCo);
+        fiveDay(latiCo,longCo,whatDateSplit);
 
     });
 
